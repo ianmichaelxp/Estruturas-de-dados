35176906fb7a3a34b684a1ad19fad89267cc35d0
Ian Michael <37078140+ianmichaelxp@users.noreply.github.com>
2019-10-24 16:55:49 -0300

    main

---
+++ src/ed_pilha/PilhaApp.java
@@ -0,0 +1,18 @@
+package ed_pilha;
+
+public class PilhaApp {
+
+	public static void main(String[] args) {
+		Pilha pilha = new Pilha();
+		pilha.push(1);
+		pilha.push(2);
+		pilha.push(3);
+		pilha.push(4);
+		Object o = pilha.pop();
+
+		System.out.println(pilha.toString() + "\n" + "pop: " + o);
+		System.out.println(pilha.top());
+		
+	}
+
+}
