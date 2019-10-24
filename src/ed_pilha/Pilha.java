c390f8814fd7e9728906c5ea5484ed6ddb433d25
Ian Michael <37078140+ianmichaelxp@users.noreply.github.com>
2019-10-24 16:02:48 -0300

    dobraArray()

--- src/ed_pilha/Pilha.java
+++ src/ed_pilha/Pilha.java
@@ -1,5 +1,20 @@
 package ed_pilha;
 
-public class Pilha {
+public class Pilha 
+{
+	private static final int ALOCACAO_INICIAL = 3;
+	private Object[] pilha = new Object[ALOCACAO_INICIAL];
+	private int inseridos;
 
+	public void dobrarArray() 
+	{	
+		Object[] novoArray = new Object[pilha.length * 2];
+		for(int i = 0; i < pilha.length; i++) 
+		{
+			novoArray[i] = pilha[i];
+		}	
+		pilha = novoArray;
+	}
+	
+	
 }
